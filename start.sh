99daa04c0522fe002c84f9651bbabbad7a186ac5
Thearenaah <thearenaah@gmail.com>
2026-06-09 11:54:58 +0100

    fix: add public/storage symlink for media URL resolution

--- start.sh
+++ start.sh
@@ -21,6 +21,7 @@ if [ ! -L /var/www/html/public/storage ]; then
     ln -sf /var/www/html/storage/app/public /var/www/html/public/storage
 fi
 
+chmod -R 777 /var/www/html/storage/framework/cache
 chmod -R 777 /var/www/html/storage
 chmod -R 777 /data/storage
 
